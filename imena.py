538a520b778d7574e8812746e43e05c554a90d2c
drag-unov <90486781+drag-unov@users.noreply.github.com>
2021-09-13 00:31:09 +0200

    Update imena.py

--- imena.py
+++ imena.py
@@ -3,11 +3,6 @@ from bs4 import BeautifulSoup
 from time import sleep
 import os
 
-URL = "https://www.mojnovisad.com/tag/imena-beba/"
-BASE_URL = "https://www.mojnovisad.com"
-trazeni_deo_naslova = "MATIČNA KNJIGA ROĐENIH"
-prikupljena_imena = {}
-
 def pokupi_imena(stranica):
     page = requests.get(stranica)
     soup = BeautifulSoup(page.content, "html.parser", from_encoding="utf-8")
@@ -23,18 +18,29 @@ def pokupi_imena(stranica):
         ime = ime.strip().capitalize()
         if not any(x in ime.upper() for x in ['TROJKE' ,'BLIZANCI', 'DEČACI','DEVOJČICE']):
             prikupljena_imena[ime] = prikupljena_imena.get(ime, 0) + 1
-            
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-clanci = soup.find_all("div", {"class": "highlight"})
-for brojac, clanak in enumerate(clanci):
-    if clanak.text.strip()[:22] == trazeni_deo_naslova:
-        stranica = BASE_URL + clanak.find_next('a')['href']
-        pokupi_imena(stranica)
-        sleep(1)
-        print(f'{brojac}.Radim stranicu: {stranica}')
 
-prikupljena_imena = dict(sorted(prikupljena_imena.items(), key=lambda item: item[1], reverse=True))
+def pronadji_stranice_sa_imenima():
+    URL = "https://www.mojnovisad.com/tag/imena-beba/"
+    BASE_URL = "https://www.mojnovisad.com"
+    trazeni_deo_naslova = "MATIČNA KNJIGA ROĐENIH"
 
-with open(os.path.expanduser('~/Desktop/imena.csv'), 'w', encoding="utf-8") as f:
-    [f.write('{0},{1}\n'.format(key, value)) for key, value in prikupljena_imena.items()]
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    clanci = soup.find_all("div", {"class": "highlight"})
+    for brojac, clanak in enumerate(clanci):
+        if clanak.text.strip()[:22] == trazeni_deo_naslova:
+            stranica = BASE_URL + clanak.find_next('a')['href']
+            pokupi_imena(stranica)
+            sleep(1)
+            print(f'{brojac}.Radim stranicu: {stranica}')
+
+def napravi_csv(putanja, imena):
+    with open(os.path.expanduser(putanja), 'w', encoding="utf-8") as f:
+        [f.write('{0},{1}\n'.format(key, value)) for key, value in imena.items()]
+
+if __name__ == "__main__":
+    prikupljena_imena = {}
+    pronadji_stranice_sa_imenima()
+    prikupljena_imena = dict(sorted(prikupljena_imena.items(), key=lambda item: item[1], reverse=True))
+    napravi_csv('~/Desktop/imena.csv', prikupljena_imena)
+ 
